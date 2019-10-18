e7899339ecbf3af8b1d68d8c822028fc5a10b1ec
chardbres <richard.m.bresnahan@gmail.com>
2019-10-18 16:45:44 -0400

    Adjusted whiskey_serializer to match examples.

--- app/serializers/whiskey_serializer.rb
+++ app/serializers/whiskey_serializer.rb
@@ -1,3 +1,7 @@
 class WhiskeySerializer < ActiveModel::Serializer
-  attributes :id, :name, :variety, :region, :age, :primary_taste, :price
+  attributes :id, :name, :variety, :region, :age, :primary_taste, :price, :editable
+
+  def editable
+    scope == object.user
+  end
 end
