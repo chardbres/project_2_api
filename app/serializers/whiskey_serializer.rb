e0e78f93a51cf753a7c4219bad3112be3344f6ba
chardbres <richard.m.bresnahan@gmail.com>
2019-10-21 10:28:39 -0400

    Removed spurious ':type' reference in the whiskey serializer.

--- app/serializers/whiskey_serializer.rb
+++ app/serializers/whiskey_serializer.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class WhiskeySerializer < ActiveModel::Serializer
-  attributes :id, :name, :type, :region, :age, :taste, :price, :editable
+  attributes :id, :name, :variety, :region, :age, :taste, :price, :editable
 
   def editable
     scope == object.user
